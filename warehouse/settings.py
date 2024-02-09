9e7918ba3affb03c29fafafcf95ed3a60faf60b0
Sandra R <sandra_rogacka_dr@wp.pl>
2024-02-09 12:32:20 +0100

    database_change_to_mongodb

--- warehouse/settings.py
+++ warehouse/settings.py
@@ -78,8 +78,10 @@ WSGI_APPLICATION = 'warehouse.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'Warehouse',
+        'HOST': 'localhost',
+        'PORT': 27017,
     }
 }
 
